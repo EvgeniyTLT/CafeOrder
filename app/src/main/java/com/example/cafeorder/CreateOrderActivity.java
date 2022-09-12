4fe7e035a967d013856b0a6fa7ced01b8b13a98d
EvgeniyTLT <Toy444@ya.ru>
2022-09-13 00:41:34 +0300

    add

--- app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -3,6 +3,8 @@ package com.example.cafeorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CreateOrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CreateOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_order);
+    }
+
+    public void onClickCreateOrder(View view) {
+        Toast.makeText(this, "Работает кнопка", Toast.LENGTH_SHORT).show();
     }
 }
