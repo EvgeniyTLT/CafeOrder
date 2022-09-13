b3b5b8aa12ef4a33229b77a1411bb1e08981386b
EvgeniyTLT <Toy444@ya.ru>
2022-09-13 23:50:19 +0300

    add

--- app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -2,19 +2,54 @@ package com.example.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateOrderActivity extends AppCompatActivity {
+
+    private TextView textViewHello;
+    private TextView textViewAdditions;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxLemon;
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
+    private String name;
+    private String password;
+    private String drink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_order);
+        Intent intent = getIntent();
+        if (intent.hasExtra("name") && intent.hasExtra("password")) {
+            name = intent.getStringExtra("name");
+            password = intent.getStringExtra("password");
+        } else {
+            name = getString(R.string.default_name);
+            password = getString(R.string.default_password);
+        }
+        drink = getString(R.string.tea);
+        textViewHello = findViewById(R.id.textViewHello);
+        textViewAdditions = findViewById(R.id.textViewAddition);
+        checkBoxMilk = findViewById(R.id.checkboxMilk);
+        checkBoxSugar = findViewById(R.id.checkboxSugar);
+        checkBoxLemon = findViewById(R.id.checkboxLemon);
+        spinnerTea = findViewById(R.id.spinnerTea);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
     }
 
-    public void onClickCreateOrder(View view) {
-        Toast.makeText(this, "Работает кнопка", Toast.LENGTH_SHORT).show();
+    public void onClickChangeDrink(View view) {
+    }
+
+    public void onClickSendOrder(View view) {
     }
 }
