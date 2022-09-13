03d780ff912a911aead7832236d4eabe310e6b3e
EvgeniyTLT <Toy444@ya.ru>
2022-09-14 01:18:19 +0300

    add

--- app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
+++ app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -89,7 +89,7 @@ public class CreateOrderActivity extends AppCompatActivity {
         String order = String.format(getString(R.string.order), name, password, drink, optionOfDrink);
         String additions;
         if (builderAdditions.length() > 0) {
-            additions = getString(R.string.need_additions) + builderAdditions.toString();
+            additions = "\n" + getString(R.string.need_additions) + builderAdditions.toString();
         } else additions = "";
         String fullOrder = order + additions;
         Intent intent = new Intent(this, OrderDetailActivity.class);
